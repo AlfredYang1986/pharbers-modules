035ba8e761564981f72c0fbd3b7c6c9bceba18cc
clock <2567729201@qq.com>
2017-10-23 11:58:48 +0800

    小bug

--- pharbers-page-memory/src/test/scala/pfizer/PfizerSuite.scala
+++ pharbers-page-memory/src/test/scala/pfizer/PfizerSuite.scala
@@ -1,7 +1,7 @@
 package pfizer
 
 import org.scalatest.FunSuite
-import com.pharbers.memory.pages.page2map
+import com.pharbers.memory.pages.{pageMemory}
 
 /**
   * Created by clock on 17-9-7.
@@ -9,15 +9,14 @@ import com.pharbers.memory.pages.page2map
 class PfizerSuite extends FunSuite {
 
     test("page memory") {
-        val t = page2map("files/test.cache")
-        println(s"t.size = ${t.allLength}")
-        println(s"t.pageCount = ${t.pageCount}")
+        val t = pageMemory("files/test.cache")
+        println(t.allData.length)
 
 //        t.allData.zipWithIndex.foreach { x =>
 //            println(s"${x._2} : ${x._1}")
 //        }
 
-        t.pageData(8).zipWithIndex.foreach { x =>
+        t.pageData(1).zipWithIndex.foreach { x =>
             println(s"${x._2} : ${x._1}")
         }
     }
