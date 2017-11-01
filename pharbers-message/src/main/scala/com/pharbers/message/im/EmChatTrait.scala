64361020b1027fb55c27f09d93f5975df35f621f
钱鹏 <891366402@qq.com>
2017-11-01 17:01:12 +0800

    环信消息新增

--- pharbers-message/src/main/scala/com/pharbers/message/im/EmChatTrait.scala
+++ pharbers-message/src/main/scala/com/pharbers/message/im/EmChatTrait.scala
@@ -1,24 +1,52 @@
 package com.pharbers.message.im
 
-import io.swagger.client.api.{AuthenticationApi, MessagesApi}
-import io.swagger.client.model.{Msg, MsgContent, Token, UserName}
+import io.swagger.client.api.{AuthenticationApi, ChatRoomsApi, MessagesApi, UsersApi}
+import io.swagger.client.model._
 import play.api.libs.json.{JsValue, Json}
 
 sealed trait EmChatTrait extends InstantMessageTrait {
 	object access {var flag: Map[String, Any] = Map.empty}
-	protected val userList: UserName
 	protected val msgObj: Msg
+	protected val roomObj: Chatroom
 	protected val msgContentObj: MsgContent
 	protected val imConf: InstantMessageInfo = queryMessageInstance("im").asInstanceOf[InstantMessageInfo]
 	
 	override def sendMsg(msg: String): String = {
 		val reVal = msgObj.msg(msgContentObj.msg(msg))
-		invokeEasemobAPI(reVal)
+		new MessagesApi().orgNameAppNameMessagesPost(imConf.AppKey, imConf.AppName, getAccessToken, reVal)
 	}
 	
-	protected def invokeEasemobAPI(payload: Msg): String = {
-		new MessagesApi().orgNameAppNameMessagesPost(imConf.AppKey, imConf.AppName, getAccessToken, payload)
+	override def createChatRoom: String =
+		new ChatRoomsApi().orgNameAppNameChatroomsPost(imConf.AppKey, imConf.AppName, getAccessToken, roomObj)
+	
+	override def deleteChatRoom(roomId: String): String =
+		new ChatRoomsApi().orgNameAppNameChatroomsChatroomIdDelete(imConf.AppKey, imConf.AppName, getAccessToken, roomId)
+	
+	override def setRoomMembers(roomId: String, userList: List[String]): String = {
+		val users = new UserName
+		val userLst = new UserNames
+		userList foreach { x =>
+			users.add(x)
+			userLst.usernames(users)
+		}
+		new ChatRoomsApi().orgNameAppNameChatroomsChatroomIdUsersPost(imConf.AppKey, imConf.AppName, getAccessToken, roomId, userLst)
 	}
+	
+	override def getAllRooms: String =
+		new ChatRoomsApi().orgNameAppNameChatroomsGet(imConf.AppKey, imConf.AppName, getAccessToken)
+	
+	override def getRoomDetail(roomId: String): String =
+		new ChatRoomsApi().orgNameAppNameChatroomsChatroomIdGet(imConf.AppKey, imConf.AppName, getAccessToken, roomId)
+	
+	override def registerUser(name: String, pwd: String): String = {
+		val users = new RegisterUsers()
+		val user = new User().username(name).password(pwd)
+		users.add(user)
+		new UsersApi().orgNameAppNameUsersPost(imConf.AppKey, imConf.AppName, users, getAccessToken)
+	}
+	
+	override def deleteRegisterUser(name: String): String = new UsersApi().orgNameAppNameUsersUsernameDelete(imConf.AppKey, imConf.AppName, getAccessToken, name)
+	
 	protected def getAccessToken: String = {
 
 		def initTokenByProp: Map[String, Any] = {
@@ -38,11 +66,12 @@ sealed trait EmChatTrait extends InstantMessageTrait {
 }
 
 case class EmChatMsg() extends EmChatTrait {
-	protected override val userList: UserName = new UserName
 	protected override val msgObj: Msg = new Msg
+	protected override val roomObj: Chatroom = new Chatroom
 	protected override val msgContentObj: MsgContent = new MsgContent
 	
 	def sendTargetUser(userNameLst: List[String]): EmChatMsg = {
+		val userList: UserName = new UserName
 		userNameLst foreach (x => userList.add(x))
 		msgObj.target(userList)
 		this
@@ -58,11 +87,6 @@ case class EmChatMsg() extends EmChatTrait {
 		this
 	}
 	
-	/**
-	  * 信息类型
-	  * @param enum 默认值为TXT文本类型
-	  * @return EmChatMsg this
-	  */
 	def sendMsgContentType(enum: MsgContent.TypeEnum = MsgContent.TypeEnum.TXT): EmChatMsg = {
 		msgContentObj.`type`(enum)
 		this
@@ -70,6 +94,26 @@ case class EmChatMsg() extends EmChatTrait {
 	
 	def sendMsgExt(ext: String Map String): EmChatMsg = {
 		msgObj.setExt(ext)
+		this
+	}
+	
+	def setRoomName(name: String): EmChatMsg = {
+		roomObj.setName(name)
+		this
+	}
+	
+	def setRoomDescription(context: String): EmChatMsg = {
+		roomObj.setDescription(context)
+		this
+	}
+	
+	def setRoomMaxUsers(num: Int): EmChatMsg = {
+		roomObj.setMaxusers(num)
+		this
+	}
+	
+	def setRoomOnwer(name: String): EmChatMsg = {
+		roomObj.setOwner(name)
 		this
 	}
 }
--- pharbers-message/src/test/scala/mailSuite.scala
+++ pharbers-message/src/test/scala/mailSuite.scala
@@ -8,8 +8,18 @@ import org.scalatest.FunSuite
 class mailSuite extends FunSuite {
 //	object temp extends SendMessageTrait with dbInstanceManager
 //	implicit val db: DBTrait = temp.queryDBInstance("cli").get
-//	temp.sendMailMessage("pqian@pharbers.com").sendTextMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail
-//	temp.sendTextMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail("pqian@pharbers.com")
-//	temp.sendHtmlMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail("pqian@pharbers.com")
-//	EmChatMsg().sendFromUser("project").sendTargetUser("test" :: Nil).sendTargetType("users").sendMsgContentType().sendMsgExt(Map("进度条" -> "10")).sendMsg("fuck")
+//	test("EmChatRoom Test") {
+//	    temp.sendMailMessage("pqian@pharbers.com").sendTextMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail
+//	    temp.sendTextMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail("pqian@pharbers.com")
+//	    temp.sendHtmlMail.setSubTheme("test").setContext("<h1><p style='color:red'>test</p><h1>").sendToEmail("pqian@pharbers.com")
+//	    EmChatMsg().sendFromUser("project").sendTargetUser("test" :: Nil).sendTargetType("users").sendMsgContentType().sendMsgExt(Map("进度条" -> "10")).sendMsg("fuck")
+//		val str = EmChatMsg().setRoomName("TestCalc").setRoomDescription("TestCalcDes").setRoomOnwer("project").setRoomMaxUsers(100).createChatRoom
+//		val str = EmChatMsg().setRoomMembers("31577339920385", "test" :: Nil)
+//		val str = EmChatMsg().getAllRooms
+//		val str = EmChatMsg().getRoomDetail("31577339920385")
+//		val str = EmChatMsg().deleteChatRoom("31577339920385")
+//		val str = EmChatMsg().
+//		val str = EmChatMsg().deleteRegisterUser("Faiz")
+//		println(str)
+//	}
 }
