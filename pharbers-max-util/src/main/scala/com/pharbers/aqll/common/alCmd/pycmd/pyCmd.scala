bd5f8e0305501b786286ed419dd24deb1d11d594
jeorch <jeorch@foxmail.com>
2017-11-15 14:34:37 +0800

    max_util写进maven
    max_util写进maven

---
+++ pharbers-max-util/src/main/scala/com/pharbers/aqll/common/alCmd/pycmd/pyCmd.scala
@@ -0,0 +1,10 @@
+package com.pharbers.aqll.common.alCmd.pycmd
+
+import com.pharbers.aqll.common.alCmd.alShellPythonCmdExce
+
+/**
+  * Created by qianpeng on 2017/5/13.
+  */
+case class pyCmd(pyDir: String, pyFileName: String, arg: String) extends alShellPythonCmdExce {
+	override def cmd = "python " + PyConfig(pyDir, pyFileName, Some(arg)).toArgs
+}
