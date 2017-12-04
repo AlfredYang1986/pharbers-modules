427f0ffb0b37325489e1a349a244f62fa13953af
clock <2567729201@qq.com>
2017-12-04 19:17:27 +0800

    加入redis的单元测试

--- pharbers-redis-driver/src/test/scala/pfizer/PfizerSuite.scala
+++ pharbers-redis-driver/src/test/scala/pfizer/PfizerSuite.scala
@@ -1,25 +1,33 @@
-package pfizer
-
-import com.pharbers.driver.redis.phRedisDriver
-import com.redis.RedisClient
-import org.scalatest.FunSuite
-
-/**
-  * Created by jeorch on 17-11-17.
-  */
-class PfizerSuite extends FunSuite {
-
+//package pfizer
+//
+//import com.pharbers.driver.redis.phRedisDriver
+//import com.redis.RedisClient
+//import org.scalatest.FunSuite
+//
+///**
+//  * Created by jeorch on 17-11-17.
+//  */
+//class PfizerSuite extends FunSuite {
+//
 //    test("Test Redis sadd"){
-//        val redisDriver = phRedisDriver()
+//        val phSetDriver = phRedisDriver().phSetDriver
 //        1 to 100000 foreach { i =>
-//            redisDriver.phSetDriver.sadd("segment", s"${i}String")
+//            phSetDriver.sadd("segment", s"${i}String")
 //        }
-//
-//        val result = redisDriver.phSetDriver.smembers("segment")
-//        println(s"result.size = ${result.size}")
-////        result.foreach(x => println(s"&& = ${x}"))
-//
-//        redisDriver.del("segment")
 //    }
-
-}
+//
+//    test("Test Read Redis sadd"){
+//        val phSetDriver = phRedisDriver().phSetDriver
+//
+//        val result = phSetDriver.smembers("segment")
+//        println(s"result.size = ${result.size}")
+//        result.foreach(x => println(s"&& = ${x}"))
+//    }
+//
+//    test("Test Delete Redis sadd"){
+//        val phSetDriver = phRedisDriver().phSetDriver
+//
+//        phRedisDriver().del("segment")
+//    }
+//
+//}
