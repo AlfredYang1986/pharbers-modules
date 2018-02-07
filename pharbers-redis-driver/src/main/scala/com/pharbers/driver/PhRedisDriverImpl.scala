e6fa553ec9b3f9ec7e92e40558acde96cd8f8de6
AlfredYang1986 <yangyuanpig@gmail.com>
2018-02-07 15:56:23 +0800

    redis 连接池

--- pharbers-redis-driver/src/main/scala/com/pharbers/driver/PhRedisDriverImpl.scala
+++ pharbers-redis-driver/src/main/scala/com/pharbers/driver/PhRedisDriverImpl.scala
@@ -8,128 +8,120 @@ import com.pharbers.driver.util.{redis_conn_instance, PhRedisTrait}
 trait PhRedisDriverImpl extends PhRedisTrait {
     implicit val conn_instance : redis_conn_instance
 
-    override def addString(key: Any, value: String): Unit ={
-        conn_instance.getConnection.set(key, value)
-    }
+    override def addString(key: Any, value: String): Unit =
+        conn_instance.getConnection(c => c.set(key, value))
 
-    override def addListLeft(key: Any, value: Any, values: Any*): Unit = {
-        conn_instance.getConnection.lpush(key, value, values)
-    }
+    override def addListLeft(key: Any, value: Any, values: Any*): Unit =
+        conn_instance.getConnection(c => c.lpush(key, value, values))
 
-    override def addListRight(key: Any, value: Any, values: Any*): Unit = {
-        conn_instance.getConnection.rpush(key, value, values)
-    }
+    override def addListRight(key: Any, value: Any, values: Any*): Unit =
+        conn_instance.getConnection(c => c.rpush(key, value, values))
 
-    override def addMap(key: Any, values: Map[Any, Any]): Unit = {
-        conn_instance.getConnection.hmset(key, values)
-    }
+    override def addMap(key: Any, values: Map[Any, Any]): Unit =
+        conn_instance.getConnection(c => c.hmset(key, values))
 
-    override def addMap(key: Any, mapK: Any, mapV: Any): Unit = {
-        conn_instance.getConnection.hset(key, mapK, mapV)
-    }
+    override def addMap(key: Any, mapK: Any, mapV: Any): Unit =
+        conn_instance.getConnection(c => c.hset(key, mapK, mapV))
 
-    override def addSet(key: Any, values: Set[Any]): Unit = {
-        values.foreach(v => conn_instance.getConnection.sadd(key, v))
-    }
+    override def addSet(key: Any, values: Set[Any]): Unit =
+        conn_instance.getConnection(c => values.foreach(c.sadd(key, _)))
 
-    override def addSet(key: Any, value: Any, values: Any*): Unit = {
-        conn_instance.getConnection.sadd(key, value, values)
-    }
+    override def addSet(key: Any, value: Any, values: Any*): Unit =
+        conn_instance.getConnection(c => c.sadd(key, value, values))
 
-    override def delete(key: Any, keys: Any*): Long = {
-        conn_instance.getConnection.del(key, keys) match {
+    override def delete(key: Any, keys: Any*) : Long = {
+        conn_instance.getConnection(c => c.del(key, keys) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Long] => value.get
-        }
+        })
     }
 
-    override def getString(key: Any): String = {
-        conn_instance.getConnection.get(key) match {
+    override def getString(key: Any) : String = {
+        conn_instance.getConnection(c => c.get(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[String] => value.get
-        }
+        })
     }
 
     override def getListAllValue(key: Any): List[String] = {
-        conn_instance.getConnection.lrange(key, 0, -1) match {
+        conn_instance.getConnection(c => c.lrange(key, 0, -1) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[List[Option[String]]] => value.get.map(x => x.get)
-        }
+        })
     }
 
     override def getListLeftValue(key: Any): String = {
-        conn_instance.getConnection.lpop(key) match {
+        conn_instance.getConnection(c => c.lpop(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[String] => value.get
-        }
+        })
     }
 
     override def getListRightValue(key: Any): String = {
-        conn_instance.getConnection.rpop(key) match {
+        conn_instance.getConnection(c => c.rpop(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[String] => value.get
-        }
+        })
     }
 
     override def getListSize(key: Any): Long = {
-        conn_instance.getConnection.llen(key) match {
+        conn_instance.getConnection(c => c.llen(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Long] => value.get
-        }
+        })
     }
 
     override def getMapAllValue(key: Any): Map[String, String] = {
-        conn_instance.getConnection.hgetall1(key) match {
+        conn_instance.getConnection(c => c.hgetall1(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Map[String, String]] => value.get
-        }
+        })
     }
 
     override def getMapSize(key: Any): Long = {
-        conn_instance.getConnection.hlen(key) match {
+        conn_instance.getConnection(c => c.hlen(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Long] => value.get
-        }
+        })
     }
 
     override def getMapValue(key: Any, mapK: Any): String = {
-        conn_instance.getConnection.hget(key, mapK) match {
+        conn_instance.getConnection(c => c.hget(key, mapK) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[String] => value.get
-        }
+        })
     }
 
     override def getSetAllValue(key: Any): Set[String] = {
-        conn_instance.getConnection.smembers(key) match {
+        conn_instance.getConnection(c => c.smembers(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Set[Option[String]]] => value.get.map(x => x.get)
-        }
+        })
     }
 
     override def getSetLastValue(key: Any): String = {
-        conn_instance.getConnection.spop(key) match {
+        conn_instance.getConnection(c => c.spop(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[String] => value.get
-        }
+        })
     }
 
     override def getSetSize(key: Any): Long = {
-        conn_instance.getConnection.scard(key) match {
+        conn_instance.getConnection(c => c.scard(key) match {
             case None => throw new Exception("cannot get this value from redis")
             case value: Option[Long] => value.get
-        }
+        })
     }
 
     override def exsits(key: Any): Boolean = {
-        conn_instance.getConnection.exists(key)
+        conn_instance.getConnection(c => c.exists(key))
     }
 
     override def expire(key: Any, ttl: Int): Unit = {
-        conn_instance.getConnection.expire(key, ttl)
+        conn_instance.getConnection(c => c.expire(key, ttl))
     }
 
     override def flush: Unit = {
-        conn_instance.getConnection.flushall
+        conn_instance.getConnection(c => c.flushall)
     }
-
 }
