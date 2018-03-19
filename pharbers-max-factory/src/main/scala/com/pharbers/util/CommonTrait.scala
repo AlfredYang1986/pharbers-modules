d7021855efb865bff5353922aa61ae895d8b314a
jeorch <jeorch@foxmail.com>
2018-03-19 12:27:39 +0800

    update interface

--- pharbers-max-factory/src/main/scala/com/pharbers/util/CommonTrait.scala
+++ pharbers-max-factory/src/main/scala/com/pharbers/util/CommonTrait.scala
@@ -1,8 +1,28 @@
 package com.pharbers.util
 
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+
 /**
   * Created by jeorch on 18-3-7.
   */
 trait CommonTrait {
+
+    def getResultFileFullPath(arg: String) : String = {
+
+        val folder = new File(arg)
+        val listFile = folder.listFiles().filter(x => x.getName.endsWith(".csv"))
+        listFile.length match {
+            case 1 => listFile.head.getAbsolutePath
+            case _ => throw new Exception("not single file")
+        }
+    }
+
+    def move2ExportFolder(originPath: String, destPath: String) = {
+        val originFile = new File(originPath)
+        val destFile = new File(destPath)
+        FileUtils.copyFile(originFile, destFile)
+    }
 
 }
