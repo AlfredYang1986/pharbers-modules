171729d96b938d7aff546f885f8e9df5b538bc19
clock <2567729201@qq.com>
2018-04-13 18:21:10 +0800

    快了，快乐！！

--- max/src/main/scala/com/pharbers/calc/phMaxCalcActionq.scala
+++ max/src/main/scala/com/pharbers/calc/phMaxCalcActionq.scala
@@ -10,9 +10,10 @@ object phMaxCalcActionq {
 
 class phMaxCalcActionq(override val defaultArgs: pActionArgs) extends pActionTrait {
     override val name: String = "max_calc_action"
-    override implicit def progressFunc(progress: Double, flag: String) : Unit = {}
 
-    override def perform(pr: pActionArgs)(implicit f: (Double, String) => Unit) : pActionArgs = {
+    override implicit def progressFunc(progress: Double, flag: String): Unit = {}
+
+    override def perform(pr: pActionArgs)(implicit f: (Double, String) => Unit): pActionArgs = {
         val sparkDriver = phSparkDriver()
         import sparkDriver.ss.implicits._
 
@@ -33,7 +34,7 @@ class phMaxCalcActionq(override val defaultArgs: pActionArgs) extends pActionTra
                     .withColumnRenamed("西药收入", "WEST_MEDICINE_INCOME")
                     .withColumnRenamed("Factor", "FACTOR")
                     .withColumnRenamed("Province", "PROVINCE")
-                    .selectExpr("PHA_ID", "IS_PANEL_HOSP", "NEED_MAX_HOSP", "SEGMENT", "WEST_MEDICINE_INCOME", "FACTOR", "PROVINCE")
+                    .selectExpr("PHA_ID", "IS_PANEL_HOSP", "NEED_MAX_HOSP", "SEGMENT", "WEST_MEDICINE_INCOME", "FACTOR", "PROVINCE", "Prefecture")
         }
 
         val panelSumed = {
@@ -50,7 +51,7 @@ class phMaxCalcActionq(override val defaultArgs: pActionArgs) extends pActionTra
                     .withColumn("HYM", concat(col("PHA_ID"), col("YM"), col("min1")))
         }
 
-        val calcData = {
+        val fullData = {
             joinData.join(panelSumed, joinData("HYM") === panelSumed("sumHYM"), "left")
                     .withColumn("s_Sales", when($"sumSales".isNull, 0.0).otherwise($"sumSales"))
                     .withColumn("s_Units", when($"sumUnits".isNull, 0.0).otherwise($"sumUnits"))
@@ -59,7 +60,7 @@ class phMaxCalcActionq(override val defaultArgs: pActionArgs) extends pActionTra
         }
 
         val groupData = {
-            calcData.filter(col("NEED_MAX_HOSP") === 1)
+            fullData.filter(col("NEED_MAX_HOSP") === 1)
                     .groupBy("SYM")
                     .agg(Map("s_Sales" -> "sum", "s_Units" -> "sum", "WEST_MEDICINE_INCOME" -> "sum"))
                     .withColumnRenamed("sum(s_Sales)", "sumSales")
@@ -67,34 +68,25 @@ class phMaxCalcActionq(override val defaultArgs: pActionArgs) extends pActionTra
                     .withColumnRenamed("sum(WEST_MEDICINE_INCOME)", "sumWestIncome")
                     .withColumn("aveSales", col("sumSales") / col("sumWestIncome"))
                     .withColumn("aveUnits", col("sumUnits") / col("sumWestIncome"))
-                    .filter(col("aveSales") =!= 0 and col("aveUnits") =!= 0)
+                    .filter(col("aveSales") > 0 and col("aveUnits") > 0)
                     .select("SYM", "aveSales", "aveUnits")
         }
 
-
-        val ta = calcData.join(groupData, calcData("SYM") === groupData("SYM")).drop("SYM")
-
         val result = {
-                    ta.withColumn("f_sales",
-                        when($"IS_PANEL_HOSP" === 1, $"sumSales").otherwise(
-                            when($"aveSales" < 0 or $"aveUnits" < 0, 0.0)
-                                    .otherwise($"aveSales" * $"WEST_MEDICINE_INCOME" * $"FACTOR")
-                        )
+            fullData.join(groupData, fullData("SYM") === groupData("SYM"), "left").drop("SYM")
+                    .withColumn("f_sales",
+                        when($"IS_PANEL_HOSP" === 1, $"sumSales")
+                                .when($"aveSales".isNotNull, $"aveSales" * $"WEST_MEDICINE_INCOME" * $"FACTOR")
+                                .otherwise(0.0)
                     )
                     .withColumn("f_units",
-                        when($"IS_PANEL_HOSP" === 1, $"sumUnits").otherwise(
-                            when($"aveSales" < 0 or $"aveUnits" < 0, 0.0)
-                                    .otherwise($"aveUnits" * $"WEST_MEDICINE_INCOME" * $"FACTOR"))
+                        when($"IS_PANEL_HOSP" === 1, $"sumUnits")
+                                .when($"aveSales".isNotNull, $"aveUnits" * $"WEST_MEDICINE_INCOME" * $"FACTOR")
+                                .otherwise(0.0)
                     )
                     .filter($"f_sales" =!= 0 and $"f_units" =!= 0)
-                    .selectExpr("min1", "PHA_ID", "f_sales", "f_units")
+                    .selectExpr("min1", "PHA_ID", "f_sales", "f_units", "IS_PANEL_HOSP")
         }
-
-        println(result.count())
-//        result.show(false)
-
-        val test = result.agg(Map("f_sales" -> "sum", "f_units" -> "sum"))
-        test.show()
 
         DFArgs(result)
     }
