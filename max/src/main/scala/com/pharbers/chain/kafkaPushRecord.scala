1cb2004895a6b81c87f82813506f5e724cabe5fa
AlfredYang1986 <yangyuanpig@gmail.com>
2018-04-18 16:05:18 +0800

    kafka puddler

--- max/src/main/scala/com/pharbers/chain/kafkaPushRecord.scala
+++ max/src/main/scala/com/pharbers/chain/kafkaPushRecord.scala
@@ -1,5 +1,51 @@
 package com.pharbers.chain
 
-trait kafkaPushRecord {
+import java.io.File
+import java.util.Properties
 
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.avro.io.EncoderFactory
+import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.commons.io.output.ByteArrayOutputStream
+import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord}
+
+trait kafkaPushRecord {
+    def pushRecord(id : String, name : String, stages : String, progress : Int)(topic : String) = {
+        val props = new Properties()
+
+        props.put("bootstrap.servers", "localhost:9092")
+        props.put("acks", "all")
+        props.put("retries", Integer.valueOf(0))
+        props.put("batch.size", Integer.valueOf(16384))
+        props.put("linger.ms", Integer.valueOf(1))
+        props.put("buffer.memory", Integer.valueOf(33554432))
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+        props.put("serializer.class", "kafka.serializer.DefaultEncoder")
+
+        val producer : Producer[String, Array[Byte]] = new KafkaProducer(props)
+
+        val schema = new Schema.Parser().parse(new File("pharbers_config/progress.arsc"))
+        val payload = new GenericData.Record(schema)
+        payload.put("id", id)
+        payload.put("name", name)
+        payload.put("stages", stages)
+        payload.put("progress", progress)
+        println(s"Original Message : " + payload)
+
+        val writer = new SpecificDatumWriter[GenericRecord](schema)
+        val out = new ByteArrayOutputStream()
+        val encoder = EncoderFactory.get().binaryEncoder(out, null)
+        writer.write(payload, encoder)
+        encoder.flush()
+        out.close()
+
+        val serializedBytes  = out.toByteArray
+        println(s"Sending message in bytes : " + serializedBytes)
+
+        val message = new ProducerRecord[String, Array[Byte]](topic, serializedBytes)
+        producer.send(message)
+        producer.close()
+    }
 }
--- max/src/test/scala/com/pharbers/kafka/KTestSuite.scala
+++ max/src/test/scala/com/pharbers/kafka/KTestSuite.scala
@@ -2,6 +2,7 @@ package com.pharbers.kafka
 
 import com.pharbers.chain.kafkaPushTopic
 import com.pharbers.chain.kafkaLstTopics
+import com.pharbers.chain.kafkaPushRecord
 import org.scalatest.FunSuite
 
 class KTestSuite extends FunSuite{
@@ -14,5 +15,10 @@ class KTestSuite extends FunSuite{
     test("kafka topic listing") {
         case class lstTopic() extends kafkaLstTopics
         lstTopic().lstTopics.foreach(println(_))
+    }
+
+    test("kafka push record to topic alfredyang") {
+        case class pushRecord() extends kafkaPushRecord
+        pushRecord().pushRecord("abcde", "abcde", "alfred", 50)("alfredyang")
     }
 }
