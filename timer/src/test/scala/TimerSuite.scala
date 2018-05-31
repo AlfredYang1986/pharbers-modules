2c0ee8061b3897d2fe86db3b02290bde4d2ca949
jeorch <jeorch@foxmail.com>
2018-05-31 18:14:02 +0800

    调整maven加载顺序

--- timer/src/test/scala/TimerSuite.scala
+++ timer/src/test/scala/TimerSuite.scala
@@ -7,8 +7,8 @@
 //
 //    test("测试TimerJob") {
 //        val date = new Date(new Date().getTime + 2 * 1000)
-//        TimerJob("com.pharbers.timer.TimerTest").start(date)
-//        TimerJob("com.pharbers.timer.TimerTest").start(5)
+//        TimerJob("com.pharbers.timer.TimerTestAction").start(date)
+//        TimerJob("com.pharbers.timer.TimerTestAction").start(5)
 //        Thread.sleep(10 * 1000)
 //        println("Done")
 //    }
