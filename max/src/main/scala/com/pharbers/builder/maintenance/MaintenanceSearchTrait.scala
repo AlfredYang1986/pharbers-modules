bc2c7ff475345544e538db16eed7db1f2bf2be38
clock <2567729201@qq.com>
2018-06-13 16:57:22 +0800

    temp

--- max/src/main/scala/com/pharbers/builder/maintenance/MaintenanceSearchTrait.scala
+++ max/src/main/scala/com/pharbers/builder/maintenance/MaintenanceSearchTrait.scala
@@ -1,16 +1,16 @@
 package com.pharbers.builder.maintenance
 
-import com.pharbers.builder.phMarketTable.{phMarketManager, phMarketDBTrait, phReflectCheck}
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
+import com.pharbers.builder.phMarketTable.phMarketManager
 
 /**
   * Created by jeorch on 18-6-6.
   */
-trait MaintenanceSearchTrait extends phReflectCheck with phMarketDBTrait with phMarketManager {
-    def getAllCompanies1: (Option[Map[String, JsValue]], Option[JsValue]) = {
-
-        (Some(Map("companies" -> toJson("getMktTableAllCompanies"))), None)
+trait MaintenanceSearchTrait extends phMarketManager {
+    override def getAllCompanies: (Option[Map[String, JsValue]], Option[JsValue]) = {
+        val a = super.getAllCompanies
+        (Some(Map("companies" -> toJson(getMktTableAllCompanies))), None)
     }
 
     def getDataCleanModuleArgs(jv: JsValue): (Option[Map[String, JsValue]], Option[JsValue]) = ???
@@ -43,5 +43,62 @@ trait MaintenanceSearchTrait extends phReflectCheck with phMarketDBTrait with ph
 //        (Some(Map("match_tables" -> toJson(matchFileLst), "universe_files" -> toJson(universeFileLst))), None)
         ???
     }
+
+
+
+
+
+
+        def getCompanyTables(company: String): List[Map[String, String]] = ???
+    //        marketTable.filter(x => company == x("company"))
+
+        def getNotCompanyTables(company: String): List[Map[String, String]] = ???
+    //        marketTable.filter(x => company != x("company"))
+
+        def getTable(company: String, market: String): Map[String, String] = ???
+    //        marketTable.find(x => company == x("company") && market == x("market")).getOrElse(throw new Exception("input wrong"))
+
+        def getSourceLst(company: String, market: String): Array[String] =
+            getTable(company, market).getOrElse("source", throw new Exception("input wrong")).split("#")
+
+        def getDataCleanArgLst(company: String, market: String): Array[String] =
+            getTable(company, market).getOrElse("dataCleanArgs", throw new Exception("input wrong")) match {
+                case "" => Array.empty[String]
+                case s => s.split("#")
+            }
+
+
+        def getPanelArgLst(company: String, market: String): Array[String] =
+            getTable(company, market).getOrElse("panelArgs", throw new Exception("input wrong")).split("#")
+
+        def getMaxArgLst(company: String, market: String): Array[String] =
+            getTable(company, market).getOrElse("maxArgs", throw new Exception("input wrong")).split("#")
+
+        def getDeliveryArgLst(company: String, market: String): Array[String] =
+            getTable(company, market).getOrElse("deliveryArgs", throw new Exception("input wrong")).split("#")
+
+        def getDataCleanArgs(company: String, market: String): Map[String, String] = {
+            val table = getTable(company, market)
+            getDataCleanArgLst(company, market) match {
+                case lst if lst.isEmpty => Map.empty
+                case lst if lst.nonEmpty => lst.map(x => x -> table(x)).toMap
+            }
+        }
+
+        def getPanelArgs(company: String, market: String): Map[String, String] = {
+            val table = getTable(company, market)
+            getPanelArgLst(company, market).map(x => x -> table(x)).toMap
+        }
+
+        def getMaxArgs(company: String, market: String): Map[String, String] = {
+            val table = getTable(company, market)
+            getMaxArgLst(company, market).map(x => x -> table(x)).toMap
+        }
+
+        def getDeliveryArgs(company: String, market: String): Map[String, String] = {
+            val table = getTable(company, market)
+            getDeliveryArgLst(company, market).map(x => x -> table(x)).toMap
+        }
+
 
 }
