102948f0a77505e7893930cb7dea439e8404a2f7
ZhangChi <jeorch@foxmail.com>
2018-06-13 14:56:29 +0800

    添加一个查询公司可以导出的格式

--- max/src/main/scala/com/pharbers/builder/search/SearchDataExport.scala
+++ max/src/main/scala/com/pharbers/builder/search/SearchDataExport.scala
@@ -1,12 +1,13 @@
 package com.pharbers.builder.search
 
+import com.pharbers.builder.MarketTable
 import com.pharbers.pactions.actionbase.{MapArgs, StringArgs}
 import com.pharbers.search.{phDeliverySearchDataJob, phExportSearchDataJob}
 import com.pharbers.spark.phSparkDriver
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json.toJson
 
-trait SearchDataExport {
+trait SearchDataExport extends MarketTable {
     def exportData(jv: JsValue): (Option[Map[String, JsValue]], Option[JsValue]) = {
         val company = (jv \ "user" \ "company" \ "company_id").asOpt[String].getOrElse(throw new Exception("Illegal company"))
         val market = (jv \ "condition" \ "market").asOpt[String].getOrElse("")
@@ -41,6 +42,17 @@ trait SearchDataExport {
         phSparkDriver().sc.stop
 
         (Some(Map("delivery_file_name" -> toJson(exportResult))), None)
+    }
+
+    def getCurrentCompanyExportType(jv: JsValue): (Option[Map[String, JsValue]], Option[JsValue]) = {
+        val company = (jv \ "user" \ "company" \ "company_id").asOpt[String].getOrElse(throw new Exception("Illegal company"))
+        val company_name = marketTable.find(company == _("company")).getOrElse(throw new Exception("You account has no calc lincense!"))("company_name")
+        val exportTypeLst: List[String] = marketTable.filter(company == _("company")).map(_("deliveryInstance")).distinct.filter(_ != "") match {
+            case Nil => "Max格式" :: Nil
+            case _ => "Max格式" :: s"${company_name}交付格式" :: Nil
+        }
+
+        (Some(Map("export_data_type" -> toJson(exportTypeLst))), None)
     }
 
 }
